29478abda3be4b62a6bdcdaa80e806bc8149a944
François-Xavier Lyonnet du Moutier <fx.du.moutier@gmail.com>
2022-08-22 16:13:28 +0200

    move file to qushape module

--- qushape/main.py
+++ qushape/main.py
@@ -1,16 +1,21 @@
-
 from PyQt4 import QtGui, QtCore
 import sys, os
 from mainWindow import *
-if __name__ == '__main__':
+
+
+def main_wrapper():
     app = QtGui.QApplication(sys.argv)
     app.setOrganizationName("Week Lab")
     app.setOrganizationDomain("http://www.chem.unc.edu/rna/")
     app.setApplicationName("QuShape")
-    app.setStyle(QtGui.QStyleFactory.create('Cleanlooks'))  
-    iconFileName=os.getcwd()+"/Icons/QuShapeIcon.png"
+    app.setStyle(QtGui.QStyleFactory.create("Cleanlooks"))
+    iconFileName = os.getcwd() + "/Icons/QuShapeIcon.png"
     app.setWindowIcon(QtGui.QIcon(iconFileName))
     form = MainWindow()
     form.show()
-  #  form.loadInitialFile()
+    #  form.loadInitialFile()
     app.exec_()
+
+
+if __name__ == "__main__":
+    main_wrapper()
